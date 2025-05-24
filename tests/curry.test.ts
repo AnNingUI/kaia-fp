9e5e75075c24964e9ab4a942522e017c89cfa409
AnNingUI <an3533581512@gmail.com>
2025-05-24 11:14:46 +0800

    Add curryVariadic curryWithDefault in curry.ts

--- tests/curry.test.ts
+++ tests/curry.test.ts
@@ -1,8 +1,19 @@
 import { describe } from "node:test";
 import { expect, it } from "vitest";
-import { _, curry } from "../src/utils/pipe/curry";
+import {
+	_,
+	curry,
+	curryVariadic,
+	curryWithDefault,
+} from "../src/utils/pipe/curry";
 const addBasic = (a: number, b: number, c: number) => a + b + c;
 const add = curry(addBasic);
+const max = curryVariadic(Math.max);
+function greet(name: string, prefix = "Hi", suffix = "!") {
+	return `${prefix} ${name}${suffix}`;
+}
+
+const cg = curryWithDefault(greet);
 
 describe("模拟实际场景测试", () => {
 	it("add", () => {
@@ -13,13 +24,18 @@ describe("模拟实际场景测试", () => {
 		const g = add(_)(2, _, 3)(1);
 		const h = add(1)(2)(3);
 		const d = add(_, 2, 3);
-		// const a = addBasic(1, 2, 3);
-		// const b = addBasic(1, 2, 3);
-		// const e = addBasic(1, 2, 3);
-		// const f = addBasic(1, 2, 3);
-		// const g = addBasic(1, 2, 3);
-		// const h = addBasic(1, 2, 3);
-		// const d = (a: number) => addBasic(a, 2, 3);
+
+		// Can not use _ in the argument
+		const u = max(1)(2)(3).exec();
+		const i = max(1, 2, 3).exec();
+		const j = cg("Alice").exec();
+		const k = cg("Bob", "Hello").exec();
+		const l = cg("Bob", "Mr.", "?").exec();
+		const m = cg(_, "Mr.", "?")("Bob").exec();
+		const n = cg("Bob")("Mr.", "?").exec();
+		const o = cg("Bob", _, "?")("Mr.").exec();
+		const p = cg("Bob", "Mr.")("?").exec();
+
 		expect(a).toBe(6);
 		expect(b).toBe(6);
 		expect(e).toBe(6);
@@ -27,5 +43,14 @@ describe("模拟实际场景测试", () => {
 		expect(g).toBe(6);
 		expect(h).toBe(6);
 		expect(d(1)).toBe(6);
+		expect(u).toBe(3);
+		expect(i).toBe(3);
+		expect(j).toBe("Hi Alice!");
+		expect(k).toBe("Hello Bob!");
+		expect(l).toBe("Mr. Bob?");
+		expect(m).toBe("Mr. Bob?");
+		expect(n).toBe("Mr. Bob?");
+		expect(o).toBe("Mr. Bob?");
+		expect(p).toBe("Mr. Bob?");
 	});
 });
